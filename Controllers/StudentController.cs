33484a3a30502babfd35268ccf29e81ea95a46ee
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2023-11-06 11:59:56 +0000

    added typed view

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<string> Students = new List<string>();
+
+            Students.Add("Rakesh");
+            Students.Add("John");
+            Students.Add("Bruce");
+
+            ViewData["students"]= Students;
+
+
+            return View();
+        }
+        public IActionResult Student()
+        {
+            Student student = new() { name = "John" , age=61};
+            return View(student);
+        }
+    }
+}
