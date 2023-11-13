d6280a26eb9ed6808f1efce9825efd77772688cf
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2023-11-13 11:57:32 +0000

    Added DBContext

---
+++ Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+
+        [DataType(DataType.Date)]// annotation
+        public DateTime ReleaseDate { get; set; }
+
+        public string? Genre { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string? Rating { get; set; }
+    }
+}
